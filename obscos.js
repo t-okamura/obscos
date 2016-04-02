e290fb28261590dccfa6d289c8e8670ac0b493ea
tokamura <awertyuikmnbvcxz@gmail.com>
2016-04-02 15:19:33 +0900

    initial

---
+++ obscos.js
@@ -0,0 +1,13 @@
+$(function(){
+    var box    = $("#menu-wrap");
+    var boxTop = box.offset().top;
+    $(window).scroll(function () {
+        if($(window).scrollTop() >= boxTop) {
+            box.addClass("fixed");
+            $("body").css("margin-top","40px");
+        } else {
+            box.removeClass("fixed");
+            $("body").css("margin-top","0px");
+        }
+    });
+});
